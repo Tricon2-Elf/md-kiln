52748b27b6c4d82e29b28d955fe95fd9146efd38
Tricon2-Elf <1232321+Tricon2-Elf@users.noreply.github.com>
2026-06-12 13:52:07 +1200

    Add compression and helmet

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const compression = require('compression');
+const helmet = require('helmet');
 const path = require('path');
 const { runStatusPlugin } = require('./plugins');
 const { loadConfig, getConfig } = require('./lib/content');
@@ -8,6 +10,23 @@ const { runBuild, startWatcher } = require('./lib/watch');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const PUBLIC_DIR = process.env.PUBLIC_DIR || path.join(__dirname, 'public');
+
+app.set('trust proxy', 1);
+app.disable('x-powered-by');
+
+app.use(compression());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        'img-src': ["'self'", 'data:', 'https:'],
+        'frame-src': ["'self'", 'https:'],
+        'upgrade-insecure-requests': null,
+      },
+    },
+  }),
+);
 
 app.get('/api/status', async (_req, res) => {
   const config = getConfig();
